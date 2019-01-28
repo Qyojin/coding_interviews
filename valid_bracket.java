874ec1092be04b7231a0c46f21fac384eb4cecbf
Qyojin <comefindjoshua@gmail.com>
2019-01-27 17:57:01 -0800

    added new problem

---
+++ valid_bracket.java
@@ -0,0 +1,18 @@
+/*
+* Givem a list of string, determine if each string is a valid bracket string.
+* If the string is valid bracket, return "YES", otherwise return "NO".
+* A bracket is considered valid if its opening and closing bracket can be matched.
+* E.g "[()]" is valid, "{}(){})" is not valid
+* 
+*/
+
+class Solution{
+
+	public static void main(String[] args){
+
+	}
+
+	public String[] validBracket(String[] strings){
+		
+	}
+}
